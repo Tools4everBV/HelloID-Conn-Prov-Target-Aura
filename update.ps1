59eab55b222870c55d6a1bae2b5c29de116db1d8
Rick Jongbloed <r.jongbloed@tools4ever.com>
2025-09-12 16:17:07 +0200

    Fix-issues-2-and-3 (#5)
    * Fix-issues-2-and-3
    
    * removed space

--- update.ps1
+++ update.ps1
@@ -130,7 +130,7 @@ function ConvertTo-AuraAccountFromXML {
         KAMERNR =           $null
         CC_EMAILADRES =     $null
         VESTIGING =         $null
-        UserPricipName =    $null
+        UserPrincipName =   $null
         password       =    $null
     }
 
